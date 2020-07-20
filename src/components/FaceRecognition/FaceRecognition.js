b7463c16b46fac391cd86d6f4d549fcd302a566d
JP.Sainsbury <JP.Sainsbury@consensys.net>
2020-07-20 16:01:37 +0100

    box2

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => {
+const FaceRecognition = ({ imageUrl, box, box2 }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
+        <div
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
+        <div
+          className='bounding-box'
+          style={{
+            top: box2.topRow,
+            right: box2.rightCol,
+            bottom: box2.bottomRow,
+            left: box2.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
-}
+};
 
 export default FaceRecognition;
